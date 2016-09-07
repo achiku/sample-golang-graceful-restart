7f5d1e779961da9c3c9c8996f70cd0f782ec59e2
Akira Chiku <akira.chiku@gmail.com>
2016-09-07 15:30:10 +0900

    Update

--- main.go
+++ main.go
@@ -6,11 +6,42 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
 	gracedown "github.com/shogo82148/go-gracedown"
 )
+
+type counter struct {
+	m     *sync.Mutex
+	count int
+}
+
+func (c *counter) Get() int {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	c.count = c.count + 1
+	return c.count
+}
+
+func newCounter() *counter {
+	return &counter{
+		m:     &sync.Mutex{},
+		count: 0,
+	}
+}
+
+func createCounterWaitHandler(c *counter) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		i := c.Get()
+		log.Printf("start # %d", i)
+		time.Sleep(10 * time.Second)
+		fmt.Fprintf(w, "hello")
+		log.Printf("done # %d", i)
+	}
+}
 
 func waitHandler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(10 * time.Second)
@@ -20,6 +51,7 @@ func waitHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/wait", waitHandler)
+	mux.HandleFunc("/count/wait", createCounterWaitHandler(newCounter()))
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGHUP)
